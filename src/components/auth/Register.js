adad4ead298322374b8a01a2d683aa7b1673bbd3
Debora Bucci <dbucci@protonmail.com>
2022-04-20 13:10:06 -0300

    create a component called 'Register' which displays a heading and the <Hero> component

---
+++ src/components/auth/Register.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "../UI/Hero";
+
+const Register = () => {
+  return (
+    <section className="register">
+      <div className="register__content">
+        <h2 className="heading--secondary">Registrarse</h2>
+      </div>
+
+      <Hero />
+    </section>
+  );
+};
+
+export default Register;
