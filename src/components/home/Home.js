c3b1bc94a1a08cd6ac24a1cb2770a84105a5f4b9
Debora Bucci <dbucci@protonmail.com>
2022-04-21 20:52:48 -0300

    add the 'Tasks' component in 'Home'

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../header/Header";
+import Tasks from "../tasks/Tasks";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Tasks />
     </div>
   );
 };
