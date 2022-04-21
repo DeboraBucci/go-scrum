9ae2391609b2b7c5c1a8a709266951e3b5775f06
Debora Bucci <dbucci@protonmail.com>
2022-04-21 13:44:09 -0300

    in the 'Register' component, add a redirection handler function to send the user to '/login', and change some of the classes' names of the elements

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Hero from "../UI/Hero";
+import RegisterForm from "./RegisterForm";
 
 const Register = () => {
+  const navigate = useNavigate();
+
+  const loginHandler = () => {
+    navigate("/login", { replace: true });
+  };
+
   return (
-    <section className="register">
-      <div className="register__content">
-        <h2 className="heading--secondary">Registrarse</h2>
+    <section className="form-container">
+      <div className="form-container__content">
+        <h2 className="heading--secondary margin-b-md">Registrarse</h2>
+        <RegisterForm loginHandler={loginHandler} />
       </div>
 
       <Hero />
