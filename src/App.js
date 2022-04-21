ec6ed54ce55a5c48791076822f029f2fc3d7f4b8
Debora Bucci <dbucci@protonmail.com>
2022-04-21 20:53:31 -0300

    create a route for the 'User' component in'App'

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 
 import Login from "./components/auth/Login.js";
 import Register from "./components/auth/Register.js";
+import User from "./components/auth/User.js";
 import Error404 from "./components/errors/Error404.js";
 import Home from "./components/home/Home.js";
 
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/user" element={<User />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </React.Fragment>
