438a042a063e7642d8e78bdeb317d8131bcd3b62
Debora Bucci <dbucci@protonmail.com>
2022-04-27 17:02:46 -0300

    create a new component called 'FieldWrapper' which will return a <div> with a label and a <Field> and <ErrorMessage> components inside, each customized through props

---
+++ src/components/UI/FieldWrapper.js
@@ -0,0 +1,24 @@
+import { ErrorMessage } from "formik";
+import React from "react";
+import TextError from "./TextError";
+
+const FieldWrapper = ({ children, text, i, name }) => {
+  return (
+    <div className="new-task__input-group">
+      <label htmlFor={name}>
+        <span className="hidden">{text}</span>
+        <i className={i}></i>
+      </label>
+
+      {children}
+
+      <ErrorMessage
+        name={name}
+        component={TextError}
+        className={"new-task__input-group--err"}
+      />
+    </div>
+  );
+};
+
+export default FieldWrapper;
