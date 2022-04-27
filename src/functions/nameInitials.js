7913d51c14fdab191ea17da0b76450c615aec5b0
Debora Bucci <dbucci@protonmail.com>
2022-04-27 20:15:07 -0300

    make the 'nameInitials' function return only one initial when the person has only one name

--- src/functions/nameInitials.js
+++ src/functions/nameInitials.js
@@ -1,4 +1,8 @@
 export const nameInitials = (fullName) => {
   const initialsArr = fullName.split(" ").map((n) => n[0]);
-  return initialsArr[0] + initialsArr[initialsArr.length - 1];
+
+  return (
+    (initialsArr.length === 1 && initialsArr[0]) ||
+    initialsArr[0] + initialsArr[initialsArr.length - 1]
+  );
 };
