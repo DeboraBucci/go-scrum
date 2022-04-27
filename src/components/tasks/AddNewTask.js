7c7bc8be5daba9175c73930b691a9bdd8d6af13e
Debora Bucci <dbucci@protonmail.com>
2022-04-27 17:05:59 -0300

    in the 'AddNewTask' component delete the everything inside the 'onSubmit' function (except for the 'closeSidebarHandler()' function call) and add a function call to send the 'values' object to its parent component. Also, reformat the code using the <FieldWrapper> component

--- src/components/tasks/AddNewTask.js
+++ src/components/tasks/AddNewTask.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { ErrorMessage, Field, Form, Formik } from "formik";
+import { Field, Form, Formik } from "formik";
 import * as Yup from "yup";
 
-import TextError from "../UI/TextError";
+import FieldWrapper from "../UI/FieldWrapper";
 
 const AddNewTask = ({ close, tasksHandler, closeSidebarHandler }) => {
   const initialValues = {
@@ -21,159 +21,145 @@ const AddNewTask = ({ close, tasksHandler, closeSidebarHandler }) => {
   });
 
   const onSubmit = (values) => {
-    // ID
-    values.id = values.name + Math.random();
-
-    // Format Members Array Properly
-    if (values.members.length !== 0) {
-      const membersArr = values.members.split(",");
-      const filteredArr = membersArr
-        .map((member) => member.trim())
-        .filter((m) => m !== "");
-      const finalArr = filteredArr.map(
-        (name) => name[0].toUpperCase() + name.slice(1).toLowerCase()
-      );
-      values.members = finalArr;
-    }
-
-    const tasksArr = JSON.parse(localStorage.getItem("tasks"));
-    tasksArr.push(values);
-    localStorage.setItem("tasks", JSON.stringify(tasksArr));
-    const newTasksArr = JSON.parse(localStorage.getItem("tasks"));
-    tasksHandler(newTasksArr);
-
+    tasksHandler(values);
     closeSidebarHandler();
   };
 
-  const nameInput = (
-    <div className="new-task__input-group">
-      <label htmlFor="name">
-        <span className="hidden">Nombre de la Tarea</span>
-        <i className="fa-solid fa-feather-pointed"></i>
-      </label>
-      <Field
-        type="text"
-        id="name"
-        name="name"
-        placeholder="Nombre de la Tarea"
-      />
-      <ErrorMessage
-        name="name"
-        component={TextError}
-        className={"new-task__input-group--err"}
-      />
-    </div>
-  );
+  const form = (
+    <Form className="new-task__form">
+      <div className="new-task__content">
+        {/* NAME INPUT */}
+        <FieldWrapper
+          name="name"
+          text="Nombre de la Tarea"
+          i="fa-solid fa-feather-pointed"
+        >
+          <Field
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Nombre de la Tarea"
+          />
+        </FieldWrapper>
 
-  const statusDropdown = (
-    <div className="new-task__input-group">
-      <label htmlFor="status">
-        <span className="hidden">Estado Actual de la Tarea</span>
-        <i className="fa-solid fa-chart-simple"></i>
-      </label>
-      <Field as="select" id="status" name="status">
-        <option defaultValue value="choose">
-          Elige el Estado de la Tarea
-        </option>
-        <option value="backlog">Backlog</option>
-        <option value="todo">To Do</option>
-        <option value="doing">Doing</option>
-        <option value="testing">Testing</option>
-        <option value="codeReview">Code Review</option>
-        <option value="done">Done</option>
-      </Field>
-      <ErrorMessage
-        name="status"
-        component={TextError}
-        className={"new-task__input-group--err"}
-      />
-    </div>
-  );
+        {/* STATUS DROPDOWN */}
+        <FieldWrapper
+          name="status"
+          text="Estado Actual de la Tarea"
+          i="fa-solid fa-chart-simple"
+        >
+          <Field as="select" id="status" name="status">
+            <option defaultValue value="choose">
+              Elige el Estado de la Tarea
+            </option>
+            <option value="backlog">Backlog</option>
+            <option value="todo">To Do</option>
+            <option value="doing">Doing</option>
+            <option value="testing">Testing</option>
+            <option value="codeReview">Code Review</option>
+            <option value="done">Done</option>
+          </Field>
+        </FieldWrapper>
 
-  const difficultyDropdown = (
-    <div className="new-task__input-group">
-      <label htmlFor="difficulty">
-        <span className="hidden">Dificultad de la Tarea</span>
-        <i className="fa-solid fa-layer-group"></i>
-      </label>
-      <Field as="select" id="difficulty" name="difficulty">
-        <option defaultValue value="">
-          Dificultad de la Tarea
-        </option>
-        <option value="trivial">Trivial</option>
-        <option value="easy">Fácil</option>
-        <option value="medium">Mediana</option>
-        <option value="hard">Difícil</option>
-        <option value="epic">Épica</option>
-      </Field>
-    </div>
-  );
+        {/* DIFFICULTY DROPDOWN */}
+        <FieldWrapper
+          name="difficulty"
+          text="Dificultad de la Tarea"
+          i="fa-solid fa-layer-group"
+        >
+          <Field as="select" id="difficulty" name="difficulty">
+            <option defaultValue value="">
+              Dificultad de la Tarea
+            </option>
+            <option value="trivial">Trivial</option>
+            <option value="easy">Fácil</option>
+            <option value="medium">Mediana</option>
+            <option value="hard">Difícil</option>
+            <option value="epic">Épica</option>
+          </Field>
+        </FieldWrapper>
 
-  const descriptionInput = (
-    <div className="new-task__input-group">
-      <label htmlFor="description">
-        <span className="hidden">Descripción</span>
-        <i className="fa-solid fa-quote-left"></i>
-      </label>
-      <Field
-        as="textarea"
-        rows="5"
-        cols="33"
-        id="description"
-        name="description"
-        placeholder="Descripción"
-      />
-    </div>
-  );
+        {/* TIME INPUTS  */}
+        <div className="new-task__input-group">
+          <i className="fa-regular fa-clock"></i>
 
-  const membersInput = (
-    <div className="new-task__input-group">
-      <label htmlFor="members">
-        <span className="hidden">Miembros</span>
-        <i className="fa-solid fa-people-group"></i>
-      </label>
-      <Field
-        as="textarea"
-        id="members"
-        name="members"
-        placeholder="José Gonzales, Daniela Mendoza, ..."
-      />
-    </div>
-  );
+          <div className="new-task__input-group--time">
+            <label htmlFor="hours">
+              <span className="hidden">
+                Minutos que lleva realizar la tarea
+              </span>
+            </label>
+            <Field
+              type="number"
+              id="hours"
+              name="hours"
+              placeholder="hours"
+              min="0"
+              className="num-input"
+            />
 
-  const timeInput = (
-    <div className="new-task__input-group">
-      <i className="fa-regular fa-clock"></i>
+            <span>:</span>
 
-      <div className="new-task__input-group--time">
-        <label htmlFor="hours">
-          <span className="hidden">Minutos que lleva realizar la tarea</span>
-        </label>
-        <Field
-          type="number"
-          id="hours"
-          name="hours"
-          placeholder="hours"
-          min="0"
-          className="num-input"
-        />
+            <label htmlFor="minutes">
+              <span className="hidden">
+                Minutos que lleva realizar la tarea
+              </span>
+            </label>
+            <Field
+              type="number"
+              id="minutes"
+              name="minutes"
+              placeholder="minutes"
+              min="1"
+              max="59"
+              className="num-input"
+            />
+          </div>
+        </div>
 
-        <span>:</span>
+        {/* DESCRIPTION INPUT  */}
+        <FieldWrapper
+          name="description"
+          text="Descripción"
+          i="fa-solid fa-quote-left"
+        >
+          <Field
+            as="textarea"
+            rows="5"
+            cols="33"
+            id="description"
+            name="description"
+            placeholder="Descripción"
+          />
+        </FieldWrapper>
 
-        <label htmlFor="minutes">
-          <span className="hidden">Minutos que lleva realizar la tarea</span>
-        </label>
-        <Field
-          type="number"
-          id="minutes"
-          name="minutes"
-          placeholder="minutes"
-          min="1"
-          max="59"
-          className="num-input"
-        />
+        {/* MEMBERS INPUT  */}
+        <FieldWrapper
+          name="members"
+          text="Miembros"
+          i="fa-solid fa-people-group"
+        >
+          <Field
+            as="textarea"
+            id="members"
+            name="members"
+            placeholder="José Gonzales, Daniela Mendoza, ..."
+          />
+        </FieldWrapper>
       </div>
-    </div>
+
+      <div className="new-task__cta">
+        <button
+          type="submit"
+          className="button button--white new-task__cta--create"
+        >
+          Crear
+        </button>
+        <button type="button" className="button button--cancel" onClick={close}>
+          Cancelar
+        </button>
+      </div>
+    </Form>
   );
 
   return (
@@ -182,36 +168,7 @@ const AddNewTask = ({ close, tasksHandler, closeSidebarHandler }) => {
       validationSchema={validationSchema}
       onSubmit={onSubmit}
     >
-      <Form className="new-task__form">
-        <h2 className="heading--secondary new-task__heading">
-          Crear Nueva Tarea
-        </h2>
-
-        <div className="new-task__content">
-          {nameInput}
-          {statusDropdown}
-          {difficultyDropdown}
-          {timeInput}
-          {descriptionInput}
-          {membersInput}
-        </div>
-
-        <div className="new-task__cta">
-          <button
-            type="submit"
-            className="button button--white new-task__cta--create"
-          >
-            Crear
-          </button>
-          <button
-            type="button"
-            className="button button--cancel"
-            onClick={close}
-          >
-            Cancelar
-          </button>
-        </div>
-      </Form>
+      {form}
     </Formik>
   );
 };
