c88d9c752c5a7e55074f37ef00a9ba334ba10f90
Debora Bucci <dbucci@protonmail.com>
2022-04-27 17:07:13 -0300

    create a 'data' file with information about the users

---
+++ src/data.js
@@ -0,0 +1,31 @@
+const user1 = {
+  name: "Pedro García",
+  role: "Team Member",
+  email: "pedrogarcia@gmail.com",
+  pw: "pedro123",
+};
+
+const user2 = {
+  name: "Sarah Perez",
+  role: "Team Member",
+  email: "sarahperez@gmail.com",
+  pw: "sarah123",
+};
+
+const user3 = {
+  name: "José Andrés Montereal",
+  role: "Tech Leader",
+  email: "josemontereal@gmail.com",
+  pw: "jose123",
+};
+
+const user4 = {
+  name: "Rodrigo Gastón Diaz",
+  role: "Team Member",
+  email: "rodrigodiaz@gmail.com",
+  pw: "rodrigo123",
+};
+
+const users = [user1, user2, user3, user4];
+
+export { users };
