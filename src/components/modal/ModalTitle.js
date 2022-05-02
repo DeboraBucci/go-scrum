d7b79355e5d5b2bcc0f18b3bb68bd859b80ed185
Debora Bucci <dbucci@protonmail.com>
2022-05-02 15:24:22 -0300

    reformat and clean code inside 'ModalTitle' file

--- src/components/modal/ModalTitle.js
+++ src/components/modal/ModalTitle.js
@@ -4,44 +4,47 @@ const ModalTitle = ({ curTask, saveHandler }) => {
   const [titleEdit, setTitleEdit] = useState(false);
   const titleRef = useRef(null);
 
-  const titleChangeHandler = () => {
-    setTitleEdit(true);
-  };
-
-  const closeEditTitleHandler = () => {
-    setTitleEdit(false);
-  };
+  // FUNCTIONS -------------------------------------------------------------
+  const titleChangeHandler = () => setTitleEdit(true);
+  const closeEditTitleHandler = () => setTitleEdit(false);
 
   const saveTitleHandler = () => {
     closeEditTitleHandler(false);
 
+    // handle input
     const curTitle = titleRef.current.value;
     if (curTitle.length > 0 && curTitle.length < 20) {
       curTask.name = titleRef.current.value;
       saveHandler();
     }
   };
+  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-  return (
-    <div className="modal__title">
-      {titleEdit ? (
-        <div className="modal__t-group">
-          <input defaultValue={curTask.name} ref={titleRef} />
-          <button className="modal__t-group--save" onClick={saveTitleHandler}>
-            <i className="fa-solid fa-check"></i>
-          </button>
-          <button
-            className="modal__t-group--close"
-            onClick={closeEditTitleHandler}
-          >
-            <i className="fa-solid fa-xmark"></i>
-          </button>
-        </div>
-      ) : (
-        <h4 onDoubleClick={titleChangeHandler}>{curTask.name}</h4>
-      )}
+  // CONTENT ---------------------------------------------------------------
+  const edit = (
+    <div className="modal-title__edit">
+      <input defaultValue={curTask.name} ref={titleRef} />
+      <button
+        className="modal-title__btn modal-title__btn--save"
+        onClick={saveTitleHandler}
+      >
+        <i className="fa-solid fa-check"></i>
+      </button>
+      <button
+        className="modal-title__btn modal-title__btn--cancel"
+        onClick={closeEditTitleHandler}
+      >
+        <i className="fa-solid fa-xmark"></i>
+      </button>
     </div>
   );
+
+  const title = <h4 onDoubleClick={titleChangeHandler}>{curTask.name}</h4>;
+
+  const content = titleEdit ? edit : title;
+  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+  return <div className="modal-title margin-b-tn">{content}</div>;
 };
 
 export default ModalTitle;
