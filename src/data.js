569c5aa23b80b40c74c9cd41deeeebfb9ffb8da2
Debora Bucci <dbucci@protonmail.com>
2022-05-05 22:12:28 -0300

    delete the users' objects, and add a variable for the required message for the inputs

--- src/data.js
+++ src/data.js
@@ -1,33 +1,3 @@
-const user1 = {
-  name: "Pedro García",
-  role: "Team Member",
-  email: "pedrogarcia@gmail.com",
-  pw: "pedro123",
-};
-
-const user2 = {
-  name: "Sarah Perez",
-  role: "Team Member",
-  email: "sarahperez@gmail.com",
-  pw: "sarah123",
-};
-
-const user3 = {
-  name: "José Andrés Montereal",
-  role: "Tech Leader",
-  email: "josemontereal@gmail.com",
-  pw: "jose123",
-};
-
-const user4 = {
-  name: "Rodrigo Gastón Diaz",
-  role: "Team Member",
-  email: "rodrigodiaz@gmail.com",
-  pw: "rodrigo123",
-};
-
-const users = [user1, user2, user3, user4];
-
 const difficultyOptions = [
   { val: "", name: "Seleccione una dificultad" },
   { val: "trivial", name: "Trivial" },
@@ -46,4 +16,6 @@ const statusOptions = [
   { val: "done", name: "Done" },
 ];
 
-export { users, difficultyOptions, statusOptions };
+const requiredMessage = "*Campo obligatorio";
+
+export { difficultyOptions, statusOptions, requiredMessage };
