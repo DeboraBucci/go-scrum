fdd0ab56aae2f8d74e6d6b7b711448fba12e91ca
Debora Bucci <dbucci@protonmail.com>
2022-05-05 21:55:03 -0300

    inside the 'RegisterForm' make the following changes:
    - change the name of the 'name' input to 'userName'.
    
    - add more inputs named: 'continent', 'region' and 'teamID'.
    
    - when the form is submitted, correct the values of some inputs if necessary and then reset the form after passing it to the 'Register' component.
    
    - use the 'AuthInputGroup' for all the fields.
    
    - add a switch to disable the 'teamID' input when unchecked.
    
    - reformat and clean the code.

--- src/components/auth/RegisterForm.js
+++ src/components/auth/RegisterForm.js
@@ -1,166 +1,60 @@
-import React from "react";
-
-import { ErrorMessage, Field, Form, Formik } from "formik";
+import React, { useState } from "react";
+import { Form, Formik } from "formik";
+import { Switch, FormControlLabel } from "@mui/material";
+import { v4 as uuidv4 } from "uuid";
 import * as Yup from "yup";
 
-import TextError from "../errors/TextError";
+import AuthInputGroup from "./AuthInputGroup";
+import { requiredMessage } from "../../data";
 
-const RegisterForm = ({ loginHandler, registerHandler }) => {
+const RegisterForm = ({ loginHandler, registerHandler, dropdownsData }) => {
+  const [isSwitchOn, setIsSwitchOn] = useState(false);
+
   const initialValues = {
-    name: "",
+    userName: "",
     email: "",
     password: "",
     passwordCheck: "",
     role: "",
-    code: "",
+    continent: "",
+    region: "",
+    teamID: "",
   };
 
   const validationSchema = Yup.object({
-    name: Yup.string()
-      .required("*Requerido")
-      .min(4, "El nombre de usuario debe tener al menos 4 (cuatro) caracteres.")
-      .matches(/^([^0-9]*)$/, "El email no debe contener ningún número."),
-    email: Yup.string().email("Invalid email format").required("*Requerido"),
+    userName: Yup.string()
+      .required(requiredMessage)
+      .min(4, "El nombre debe tener al menos 4 caracteres.")
+      .matches(/^([^0-9]*)$/, "El nombre de usuario no debe contener números."),
+    email: Yup.string().email("Invalid email format").required(requiredMessage),
     password: Yup.string()
       .min(6, "La contraseña debe tener al menos 6 (seis) caracteres.")
-      .required("*Requerido")
+      .required(requiredMessage)
       .matches(
         /(?=.*\d)(?=.*[A-Z])/,
         "La contraseña debe contar con al menos una letra en mayúsculas y un número."
       ),
     passwordCheck: Yup.string()
-      .required("*Requerido")
+      .required(requiredMessage)
       .oneOf([Yup.ref("password"), null], "Las contraseñas no son idénticas."),
+    role: Yup.string().required(requiredMessage),
+    continent: Yup.string().required(requiredMessage),
+    region: Yup.string().when("continent", {
+      is: "America",
+      then: Yup.string().required(requiredMessage),
+    }),
   });
 
-  const onSubmit = (values) => {
+  const onSubmit = (values, { resetForm }) => {
+    if (values.teamID === "") values.teamID = uuidv4();
+    if (values.continent !== "America") values.region = "Otro";
+    if (values.region === "Brasil") values.region = "Brazil";
+
     registerHandler(values);
+    resetForm();
   };
 
-  const nameInput = (
-    <>
-      <div className="form__input-group">
-        <label htmlFor="name">
-          <span className="hidden">Nombre</span>
-          <i className="fa-regular fa-user"></i>
-        </label>
-        <Field
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Nombre de Usuario"
-        />
-      </div>
-      <ErrorMessage name="name" component={TextError} />
-    </>
-  );
-
-  const emailInput = (
-    <>
-      <div className="form__input-group">
-        <label htmlFor="email">
-          <span className="hidden">Email</span>
-          <i className="fa-regular fa-at"></i>
-        </label>
-        <Field
-          type="text"
-          id="email"
-          name="email"
-          placeholder="Correo Electrónico"
-        />
-      </div>
-      <ErrorMessage name="email" component={TextError} />
-    </>
-  );
-
-  const passwordInput = (
-    <>
-      <div className="form__input-group">
-        <label htmlFor="password">
-          <span className="hidden">Contraseña</span>
-          <i className="fa-solid fa-key"></i>
-        </label>
-        <Field
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Contraseña"
-        />
-      </div>
-      <ErrorMessage name="password" component={TextError} />
-    </>
-  );
-
-  const checkPasswordInput = (
-    <>
-      <div className="form__input-group">
-        <label htmlFor="passwordCheck">
-          <span className="hidden">Confirmar Contraseña</span>
-          <i className="fa-solid fa-key"></i>
-        </label>
-        <Field
-          type="password"
-          id="passwordCheck"
-          name="passwordCheck"
-          placeholder="Confirmar Contraseña"
-        />
-      </div>
-      <ErrorMessage name="passwordCheck" component={TextError} />
-    </>
-  );
-
-  const roleDropdown = (
-    <>
-      <div className="form__input-group form__dropdown">
-        <label htmlFor="role">
-          <span className="hidden">Rol en el Equipo</span>
-          <i className="fa-solid fa-people-group"></i>
-        </label>
-        <Field as="select" id="role" name="role">
-          <option defaultValue value="choose">
-            Elige tu Rol en el Equipo
-          </option>
-          <option value="leader">Líder (tech leader)</option>
-          <option value="member">Miembro (team member)</option>
-        </Field>
-        <i className="fa-solid fa-chevron-down form__dropdown--i"></i>
-      </div>
-      <ErrorMessage name="role" component={TextError} />
-    </>
-  );
-
-  const codeInput = (
-    <>
-      <div className="form__input-group">
-        <label htmlFor="code">
-          <span className="hidden">Código de Usuario</span>
-          <i className="fa-solid fa-hashtag"></i>
-        </label>
-        <Field
-          type="text"
-          id="code"
-          name="code"
-          placeholder="Código de Usuario"
-        />
-      </div>
-      <ErrorMessage name="nombre" component={TextError} />
-    </>
-  );
-
-  const ctaButtons = (
-    <div className="form__cta">
-      <button type="submit" className="button button--purple">
-        Registrarse
-      </button>
-      <button
-        type="button"
-        className="button button--underline"
-        onClick={loginHandler}
-      >
-        Log In
-      </button>
-    </div>
-  );
+  const switchChangeHandler = (e) => setIsSwitchOn(e.target.checked);
 
   return (
     <Formik
@@ -168,15 +62,145 @@ const RegisterForm = ({ loginHandler, registerHandler }) => {
       validationSchema={validationSchema}
       onSubmit={onSubmit}
     >
-      <Form className="form">
-        <div>{nameInput}</div>
-        <div>{emailInput}</div>
-        <div>{passwordInput}</div>
-        <div>{checkPasswordInput}</div>
-        <div>{roleDropdown}</div>
-        <div>{codeInput}</div>
-        <div>{ctaButtons}</div>
-      </Form>
+      {({ values }) => {
+        return (
+          <Form className="register__form">
+            {/* USERNAME */}
+            <div className="register__box register__box--1">
+              <AuthInputGroup
+                label="Nombre de Usuario"
+                icon="fa-regular fa-user"
+                name="userName"
+              />
+            </div>
+
+            {/* EMAIL */}
+            <div className="register__box register__box--2">
+              <AuthInputGroup
+                label="Correo Electrónico"
+                icon="fa-regular fa-at"
+                name="email"
+              />
+            </div>
+
+            {/* PASSWORD */}
+            <div className="register__box register__box--3">
+              <AuthInputGroup
+                label="Contraseña"
+                icon="fa-solid fa-key"
+                name="password"
+              />
+            </div>
+
+            {/* PASSWORD CHECK */}
+            <div className="register__box register__box--4">
+              <AuthInputGroup
+                label="Confirmar Contraseña"
+                icon="fa-solid fa-key"
+                name="passwordCheck"
+              />
+            </div>
+
+            {/* CONTINENT */}
+            <div className="register__box register__box--5">
+              <AuthInputGroup
+                as="select"
+                label="Seleccione su continente"
+                icon="fa-solid fa-earth-americas"
+                name="continent"
+              >
+                <option defaultValue value="">
+                  Seleccione su continente
+                </option>
+                {dropdownsData?.continente?.map((opt) => (
+                  <option key={opt} value={opt}>
+                    {opt}
+                  </option>
+                ))}
+              </AuthInputGroup>
+            </div>
+
+            {/* ROLE */}
+            <div className="register__box register__box--6">
+              <AuthInputGroup
+                as="select"
+                label="Rol en el Equipo"
+                icon="fa-solid fa-people-group"
+                name="role"
+              >
+                <option defaultValue value="" className="special-select">
+                  Seleccione su Rol en el Equipo
+                </option>
+                {dropdownsData?.Rol?.map((opt) => (
+                  <option key={opt} value={opt}>
+                    {opt}
+                  </option>
+                ))}
+              </AuthInputGroup>
+            </div>
+
+            {/* REGION */}
+            <div
+              className={`register__box register__box--7 ${
+                values.continent !== "America" && "hidden-input"
+              }`}
+            >
+              <AuthInputGroup
+                as="select"
+                label="Seleccione su región"
+                icon="fa-solid fa-location-dot"
+                name="region"
+              >
+                <option defaultValue value="">
+                  Seleccione su región
+                </option>
+                {dropdownsData?.region?.map((opt) => (
+                  <option key={opt} value={opt}>
+                    {opt}
+                  </option>
+                ))}
+              </AuthInputGroup>
+            </div>
+
+            {/* SWITCH */}
+            <div className="register__box register__box--8">
+              <FormControlLabel
+                control={<Switch size="big" color="secondary" />}
+                label="Ya pertenenzo a un equipo"
+                onChange={switchChangeHandler}
+              />
+            </div>
+
+            {/* TEAM ID */}
+            <div
+              className={`register__box register__box--9 ${
+                !isSwitchOn ? "disabled" : ""
+              }`}
+            >
+              <AuthInputGroup
+                label="Ingrese el ID de su equipo"
+                icon="fa-solid fa-hashtag"
+                name="teamID"
+                disabled={!isSwitchOn}
+              />
+            </div>
+
+            {/* CTA */}
+            <div className="register__box register__box--10 margin-t-md">
+              <button type="submit" className="button button--purple">
+                Registrarse
+              </button>
+              <button
+                type="button"
+                className="button button--underline"
+                onClick={loginHandler}
+              >
+                Log In
+              </button>
+            </div>
+          </Form>
+        );
+      }}
     </Formik>
   );
 };
